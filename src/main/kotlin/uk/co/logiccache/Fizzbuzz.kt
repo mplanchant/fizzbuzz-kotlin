d4585e10e14afdd1221f9dd058dca0b9aa43bd00
Matthew Planchant <matt@logiccache.co.uk>
2019-05-29 10:39:53 +0100

    Refactor.

--- src/main/kotlin/uk/co/logiccache/Fizzbuzz.kt
+++ src/main/kotlin/uk/co/logiccache/Fizzbuzz.kt
@@ -1,21 +1,18 @@
 package uk.co.logiccache
 
-object Fizzbuzz {
-
-    fun of(range: IntProgression): List<String> {
-        check(range.first > 0) { "Start of range expected to be greater than zero but was ${range.first}" }
-        check(range.last >= range.first) { "End of range was ${range.last} but expected to be greater than or equal to ${range.first}" }
-        return range.map { of(it) }
-    }
-
-    private fun of(number: Int): String {
-        return when {
-            number.divisibleBy(15) -> "Fizzbuzz"
-            number.divisibleBy(3) -> "Fizz"
-            number.divisibleBy(5) -> "Buzz"
-            else -> number.toString()
-        }
-    }
-
-    private fun Int.divisibleBy(x: Int) = this % x == 0
+fun fizzbuzz(range: IntProgression): List<String> {
+    check(range.first > 0) { "Start of range expected to be greater than zero but was ${range.first}" }
+    check(range.last >= range.first) { "End of range was ${range.last} but expected to be greater than or equal to ${range.first}" }
+    return range.map { fizzbuzz(it) }
 }
+
+private fun fizzbuzz(number: Int): String {
+    return when {
+        number.divisibleBy(15) -> "Fizzbuzz"
+        number.divisibleBy(3) -> "Fizz"
+        number.divisibleBy(5) -> "Buzz"
+        else -> number.toString()
+    }
+}
+
+private fun Int.divisibleBy(x: Int) = this % x == 0
--- src/test/kotlin/uk/co/logiccache/FizzbuzzTest.kt
+++ src/test/kotlin/uk/co/logiccache/FizzbuzzTest.kt
@@ -9,34 +9,34 @@ import io.kotlintest.specs.StringSpec
 class FizzbuzzTest : StringSpec({
 
     "start value of 0" {
-        val exception = shouldThrow<IllegalStateException> { Fizzbuzz.of(0..10) }
+        val exception = shouldThrow<IllegalStateException> { fizzbuzz(0..10) }
         exception.message should startWith("Start of range expected to be greater than zero but was 0")
     }
 
     "start value less than 0" {
-        val exception = shouldThrow<IllegalStateException> { Fizzbuzz.of(-1..0) }
+        val exception = shouldThrow<IllegalStateException> { fizzbuzz(-1..0) }
         exception.message should startWith("Start of range expected to be greater than zero but was -1")
     }
 
     "finish value less than start value" {
-        val exception = shouldThrow<IllegalStateException> { Fizzbuzz.of(10 downTo 0) }
+        val exception = shouldThrow<IllegalStateException> { fizzbuzz(10 downTo 0) }
         exception.message should startWith("End of range was 0 but expected to be greater than or equal to 10")
     }
 
     "Fizzbuzz of 1" {
-        Fizzbuzz.of(1..1) shouldBe listOf("1")
+        fizzbuzz(1..1) shouldBe listOf("1")
     }
 
     "Fizzbuzz of 1 to 3" {
-        Fizzbuzz.of(1..3) shouldBe listOf("1", "2", "Fizz")
+        fizzbuzz(1..3) shouldBe listOf("1", "2", "Fizz")
     }
 
     "Fizzbuzz of 1 to 5" {
-        Fizzbuzz.of(1..5) shouldBe listOf("1", "2", "Fizz", "4", "Buzz")
+        fizzbuzz(1..5) shouldBe listOf("1", "2", "Fizz", "4", "Buzz")
     }
 
     "Fizzbuzz of 1 to 15" {
-        Fizzbuzz.of(1..15) shouldBe listOf(
+        fizzbuzz(1..15) shouldBe listOf(
             "1",
             "2",
             "Fizz",
@@ -56,11 +56,11 @@ class FizzbuzzTest : StringSpec({
     }
 
     "Fizzbuzz of 10 to 15" {
-        Fizzbuzz.of(10..15) shouldBe listOf("Buzz", "11", "Fizz", "13", "14", "Fizzbuzz")
+        fizzbuzz(10..15) shouldBe listOf("Buzz", "11", "Fizz", "13", "14", "Fizzbuzz")
     }
 
     "Fizzbuzz of 1 to 20" {
-        Fizzbuzz.of(1..20) shouldBe listOf(
+        fizzbuzz(1..20) shouldBe listOf(
             "1",
             "2",
             "Fizz",
